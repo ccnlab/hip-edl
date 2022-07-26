320e200c7b75215f0299c494bab2f8c8fb9e5645
zycyc <alanzycyc@gmail.com>
2022-07-25 19:22:35 -0700

    clean up def_params.go

--- sims/testing_effect/def_params.go
+++ sims/testing_effect/def_params.go
@@ -22,30 +22,26 @@ var ParamSets = params.Sets{
 					"Prjn.Learn.Lrate":       "0.04",
 					"Prjn.Learn.Momentum.On": "false",
 					"Prjn.Learn.Norm.On":     "false",
-					"Prjn.Learn.WtBal.On":    "true", // counteracting hogging
-					//"Prjn.Learn.XCal.SetLLrn": "true", // bcm now avail, comment out = default LLrn
-					//"Prjn.Learn.XCal.LLrn":    "0",    // 0 = turn off BCM, must with SetLLrn = true
+					"Prjn.Learn.WtBal.On":    "true",
 				}},
 			{Sel: ".HippoCHL", Desc: "hippo CHL projections -- no norm, moment, but YES wtbal = sig better",
 				Params: params.Params{
-					"Prjn.CHL.Hebb":          "0.01", // .01 > .05? > .1?
-					"Prjn.Learn.Lrate":       "0.2",  // .2 probably better? .4 was prev default
+					"Prjn.CHL.Hebb":          "0.01",
+					"Prjn.Learn.Lrate":       "0.2",
 					"Prjn.Learn.Momentum.On": "false",
 					"Prjn.Learn.Norm.On":     "false",
 					"Prjn.Learn.WtBal.On":    "true",
 				}},
 			{Sel: ".PPath", Desc: "performant path, new Dg error-driven EcCa1Prjn prjns",
 				Params: params.Params{
-					"Prjn.Learn.Lrate":       "0.15", // err driven: .15 > .2 > .25 > .1
+					"Prjn.Learn.Lrate":       "0.15",
 					"Prjn.Learn.Momentum.On": "false",
 					"Prjn.Learn.Norm.On":     "false",
 					"Prjn.Learn.WtBal.On":    "true",
-					//"Prjn.Learn.XCal.SetLLrn": "true", // bcm now avail, comment out = default LLrn
-					//"Prjn.Learn.XCal.LLrn":    "0",    // 0 = turn off BCM, must with SetLLrn = true
 				}},
 			{Sel: "#CA1ToECout", Desc: "extra strong from CA1 to ECout",
 				Params: params.Params{
-					"Prjn.WtScale.Abs": "4.0", // 4 > 6 > 2 (fails)
+					"Prjn.WtScale.Abs": "4.0",
 				}},
 			{Sel: "#InputToECin", Desc: "one-to-one input to EC",
 				Params: params.Params{
@@ -58,55 +54,40 @@ var ParamSets = params.Sets{
 					"Prjn.Learn.Learn": "false",
 					"Prjn.WtInit.Mean": "0.9",
 					"Prjn.WtInit.Var":  "0.01",
-					"Prjn.WtScale.Rel": "0.5", // .5 = .3? > .8 (fails); zycyc test this
+					"Prjn.WtScale.Rel": "0.5",
 				}},
 			{Sel: "#DGToCA3", Desc: "Mossy fibers: strong, non-learning",
 				Params: params.Params{
-					"Prjn.Learn.Learn": "false", // learning here definitely does NOT work!
+					"Prjn.Learn.Learn": "false",
 					"Prjn.WtInit.Mean": "0.9",
 					"Prjn.WtInit.Var":  "0.01",
-					"Prjn.WtScale.Rel": "4", // err del 4: 4 > 6 > 8
-					//"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
+					"Prjn.WtScale.Rel": "4",
 				}},
-			//{Sel: "#ECinToCA3", Desc: "ECin Perforant Path",
-			//	Params: params.Params{
-			//		"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
-			//	}},
 			{Sel: "#CA3ToCA3", Desc: "CA3 recurrent cons: rel=2 still the best",
 				Params: params.Params{
-					"Prjn.WtScale.Rel": "2",   // 2 > 1 > .5 = .1
-					"Prjn.Learn.Lrate": "0.1", // .1 > .08 (close) > .15 > .2 > .04;
-					//"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
+					"Prjn.WtScale.Rel": "2",
+					"Prjn.Learn.Lrate": "0.1",
 				}},
 			{Sel: "#ECinToDG", Desc: "DG learning is surprisingly critical: maxed out fast, hebbian works best",
 				Params: params.Params{
-					"Prjn.Learn.Learn":       "true", // absolutely essential to have on! learning slow if off.
-					"Prjn.CHL.Hebb":          "0.2",  // .2 seems good
-					"Prjn.CHL.SAvgCor":       "0.1",  // 0.01 = 0.05 = .1 > .2 > .3 > .4 (listlize 20-100)
-					"Prjn.CHL.MinusQ1":       "true", // dg self err slightly better
-					"Prjn.Learn.Lrate":       "0.05", // .05 > .1 > .2 > .4; .01 less interference more learning time - key tradeoff param, .05 best for list20-100
+					"Prjn.Learn.Learn":       "true", // NoDGLearn key. Set to false for NoDGLearn
+					"Prjn.CHL.Hebb":          "0.2",
+					"Prjn.CHL.SAvgCor":       "0.1",
+					"Prjn.CHL.MinusQ1":       "true",
+					"Prjn.Learn.Lrate":       "0.05",
 					"Prjn.Learn.Momentum.On": "false",
 					"Prjn.Learn.Norm.On":     "false",
 					"Prjn.Learn.WtBal.On":    "true",
 				}},
 			{Sel: "#CA3ToCA1", Desc: "Schaffer collaterals -- slower, less hebb",
 				Params: params.Params{
-					"Prjn.CHL.Hebb":          "0.01", // .01 > .005 > .02 > .002 > .001 > .05 (crazy)
+					"Prjn.CHL.Hebb":          "0.01",
 					"Prjn.CHL.SAvgCor":       "0.4",
-					"Prjn.Learn.Lrate":       "0.1", // CHL: .1 =~ .08 > .15 > .2, .05 (sig worse)
+					"Prjn.Learn.Lrate":       "0.1",
 					"Prjn.Learn.Momentum.On": "false",
 					"Prjn.Learn.Norm.On":     "false",
 					"Prjn.Learn.WtBal.On":    "true",
-					//"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
 				}},
-			//{Sel: "#ECinToCA1", Desc: "ECin Perforant Path",
-			//	Params: params.Params{
-			//		"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
-			//	}},
-			//{Sel: "#ECoutToCA1", Desc: "ECout Perforant Path",
-			//	Params: params.Params{
-			//		"Prjn.WtScale.Abs": "1.5", // zycyc, test if abs activation was not enough
-			//	}},
 			{Sel: ".EC", Desc: "all EC layers: only pools, no layer-level",
 				Params: params.Params{
 					"Layer.Act.Gbar.L":        "0.1",
@@ -118,22 +99,21 @@ var ParamSets = params.Sets{
 			{Sel: "#DG", Desc: "very sparse = high inhibition",
 				Params: params.Params{
 					"Layer.Inhib.ActAvg.Init": "0.01",
-					"Layer.Inhib.Layer.Gi":    "3.8", // 3.8 > 3.6 > 4.0 (too far -- tanks)
+					"Layer.Inhib.Layer.Gi":    "3.8",
 				}},
 			{Sel: "#CA3", Desc: "sparse = high inhibition",
 				Params: params.Params{
 					"Layer.Inhib.ActAvg.Init": "0.02",
-					"Layer.Inhib.Layer.Gi":    "2.8", // 2.8 = 3.0 really -- some better, some worse
-					"Layer.Learn.AvgL.Gain":   "2.5", // stick with 2.5
+					"Layer.Inhib.Layer.Gi":    "2.8",
+					"Layer.Learn.AvgL.Gain":   "2.5",
 				}},
 			{Sel: "#CA1", Desc: "CA1 only Pools",
 				Params: params.Params{
 					"Layer.Inhib.ActAvg.Init": "0.1",
 					"Layer.Inhib.Layer.On":    "false",
 					"Layer.Inhib.Pool.On":     "true",
-					"Layer.Inhib.Pool.Gi":     "2.4", // 2.4 > 2.2 > 2.6 > 2.8 -- 2.4 better *for small net* but not for larger!
-					"Layer.Learn.AvgL.Gain":   "2.5", // 2.5 > 2 > 3
-					//"Layer.Inhib.ActAvg.UseFirst": "false", // first activity is too low, throws off scaling, from Randy, zycyc: do we need this?
+					"Layer.Inhib.Pool.Gi":     "2.4",
+					"Layer.Learn.AvgL.Gain":   "2.5",
 				}},
 		},
 		// NOTE: it is essential not to put Pat / Hip params here, as we have to use Base
